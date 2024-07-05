95dfc7b8f4648af7b966eee77d21d286e6cc3934
Elena-1717 <kea5599@mail.ru>
2024-07-05 21:32:41 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,17 @@
+my_dict = {'Elena':1969, 'Ilona': 1989, 'Anna': 1999}
+print(my_dict)
+my_dict['Elena'] = 1978
+print(my_dict)
+my_dict['Sem'] = 1968
+print(my_dict)
+my_dict = {'Elena':1969, 'Ilona': 1989, 'Anna': 1999}
+my_dict.update({'Anton': 2000, 'Katy': 2001})
+print(my_dict)
+del my_dict['Ilona']
+print(my_dict)
+my_set = {1,1,'Яблоко',1, 42.314}
+print(my_set)
+print(my_set.add(5))
+print(my_set)
+print(my_set.remove(1))
+print(my_set)
